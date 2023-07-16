79255db5d5e0a28cfe54acd978eaef4ab116c108
Anthony Alvarado <anthonydalvarado@gmail.com>
2023-07-16 19:29:12 -0400

    knowing what I know now makes this much easier

--- src/main/java/caponerefreshment/HomeScreen.java
+++ src/main/java/caponerefreshment/HomeScreen.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HomeScreen {
 
@@ -36,7 +38,7 @@ public class HomeScreen {
         }
     }
 
-    public void makePaymentMenu() {
+    public void addDepositMenu() {
         try {
             FileWriter fW = new FileWriter("transactions.csv", true);
             BufferedWriter bW = new BufferedWriter(fW);
@@ -71,6 +73,60 @@ public class HomeScreen {
             bW.write("\n");
             bW.flush();
             System.out.println("\nYour transaction has been saved.");
+            System.out.print("\nWould you like to make another deposit? (Y/N): ");
+            String ansChoice = scanner.next();
+            switch(ansChoice.toUpperCase()){
+                case "Y":
+                    addDepositMenu();
+                    break;
+                case "N":
+                    System.out.println("\nReturning to the Main Menu.");
+                    welcomeMenu();
+                    break;
+            }
+            fW.close();
+            bW.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void makePaymentMenu() {
+        try {
+            FileWriter fW = new FileWriter("transactions.csv", true);
+            BufferedWriter bW = new BufferedWriter(fW);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            int numOfFields = 5;
+            for (int i = 0; i < numOfFields; i++) {
+                if (i == 0) {
+                    String curDate = LocalDate.now().toString();
+                    bW.write(curDate);
+                } else if (i == 1) {
+                    LocalTime curTime = LocalTime.now();
+                    String formatTime = curTime.format(formatter);
+                    bW.write(formatTime);
+                } else if (i == 2) {
+                    scanner.nextLine();
+                    System.out.print("\nEnter description: ");
+                    String desc = scanner.nextLine();
+                    bW.write(desc);
+                } else if (i == 3) {
+                    System.out.print("Enter vendor: ");
+                    String vend = scanner.nextLine();
+                    bW.write(vend);
+                } else {
+                    System.out.print("Enter amount: $ ");
+                    double amt = scanner.nextDouble();
+                    double withdrawal = amt * -1;
+                    bW.write(Double.toString(withdrawal));
+                }
+                if (i != numOfFields - 1) {
+                    bW.write(" | ");
+                }
+            }
+            bW.write("\n");
+            bW.flush();
+            System.out.println("\nYour transaction has been saved.");
             System.out.print("\nWould you like to make another payment? (Y/N): ");
             String ansChoice = scanner.next();
             switch(ansChoice.toUpperCase()){
@@ -89,17 +145,7 @@ public class HomeScreen {
         }
     }
 
-    public void addDepositMenu() {
-        try {
-            FileWriter fW = new FileWriter("transactions.csv", true);
-            BufferedWriter bW = new BufferedWriter(fW);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void viewLedgerMenu() {
+    public void viewAllTransactions(){
         try {
             FileReader fW = new FileReader("transactions.csv");
             BufferedReader bW = new BufferedReader(fW);
@@ -119,8 +165,87 @@ public class HomeScreen {
             }
             bW.close();
             fW.close();
-        } catch (IOException e) {
+        }
+        catch (IOException e){
             throw new RuntimeException(e);
         }
     }
+
+    public void viewByMonth(){
+        try {
+            FileReader fW = new FileReader("transactions.csv");
+            BufferedReader bW = new BufferedReader(fW);
+            System.out.print("\nEnter a month (MM): ");
+            String month = scanner.next();
+            String line = null;
+            System.out.println("\nTransactions for " + month + ":\n");
+            while ((line = bW.readLine()) != null) {
+                Pattern pattern = Pattern.compile("(\\d{4}-" + month + "-\\d{2})\\s*\\|\\s*(\\d{2}:\\d{2}:\\d{2})\\s*\\|\\s*([^|]+)\\s*\\|\\s*([^|]+)\\s*\\|\\s*([-]?\\d+\\.?\\d*)");
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    String date = matcher.group(1);
+                    String time = matcher.group(2);
+                    String description = matcher.group(3);
+                    String vendor = matcher.group(4);
+                    String amount = matcher.group(5);
+                    System.out.println("Date: " + date + " | Time: " + time + " | Description: " + description + " | Vendor: " + vendor + "| Amount: " + amount);
+                }
+            }
+            bW.close();
+            fW.close();
+            welcomeMenu();
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void viewByYear(){
+        try {
+            FileReader fW = new FileReader("transactions.csv");
+            BufferedReader bW = new BufferedReader(fW);
+            System.out.print("\nEnter a year (YYYY): ");
+            String year = scanner.next();
+            String line = null;
+            System.out.println("\nTransactions for " + year + ":\n");
+            while ((line = bW.readLine()) != null) {
+                Pattern pattern = Pattern.compile("(" + year + "-\\d{2}-\\d{2})\\s*\\|\\s*(\\d{2}:\\d{2}:\\d{2})\\s*\\|\\s*([^|]+)\\s*\\|\\s*([^|]+)\\s*\\|\\s*([-]?\\d+\\.?\\d*)");
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    String date = matcher.group(1);
+                    String time = matcher.group(2);
+                    String description = matcher.group(3);
+                    String vendor = matcher.group(4);
+                    String amount = matcher.group(5);
+                    System.out.println("Date: " + date + " | Time: " + time + " | Description: " + description + " | Vendor: " + vendor + "| Amount: " + amount);
+                }
+            }
+            bW.close();
+            fW.close();
+            welcomeMenu();
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void viewLedgerMenu() {
+            System.out.println("\n1. View All Transactions");
+            System.out.println("2. View By Month");
+            System.out.println("3. View By Year");
+            System.out.print("\nEnter your choice: ");
+            String choice = scanner.next();
+            switch(choice) {
+                case "1":
+                    viewAllTransactions();
+                    break;
+                case "2":
+                    viewByMonth();
+                    break;
+                case "3":
+                    viewByYear();
+                    break;
+            }
+    }
+
 }
